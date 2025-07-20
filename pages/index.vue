7dcc5a91f0ddda847106f738ec3ce789f161db09
Raresssm <lungurares4@gmail.com>
2025-07-20 12:44:15 +0300

    Moved search logic to a shared store and fixed search functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,16 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-import type { SearchResult } from '../types/types'
+import { onMounted } from 'vue'
+import { useSearch } from '~/composables/useSearch'
 
-const movies = ref<SearchResult[] | []>([])
-const showCustomAlert = ref(false)
-const alertMessage = ref('')
-const isSearching = ref(false)
-const router = useRouter()
+const { results, loading, showCustomAlert, alertMessage, fetchPopularMovies } = useSearch()
 
-
-fetchMovies()
-
-async function fetchMovies(query = '') {
-  isSearching.value = !!query
-  let url = '/api/movies'
-  if (query) url += `?query=${encodeURIComponent(query)}`
-  try {
-    //TODO fix this to use a store. Make sure to update the search component to also use the store.
-    const {data} = await useFetch<SearchResult>(url) as any
-    if (data.value && Array.isArray(data.value.results)) {
-      if (data.value.results.length === 0 && isSearching.value) {
-        alertMessage.value = 'No results found. Going back to the front page!';
-        showCustomAlert.value = true;
-        setTimeout(() => {
-          showCustomAlert.value = false;
-          router.push('/')
-        }, 2500);
-        isSearching.value = false;
-        fetchMovies('')
-        return;
-      }
-      movies.value = data.value.results
-    } else {
-      movies.value = []
-    }
-  } catch (e) {
-    console.error('API fetch error:', e)
-    movies.value = []
-  }
-}
-
-
+onMounted(() => {
+  fetchPopularMovies()
+})
 </script>
 
 <template>
   <div>
-
     <transition name="fade">
       <div v-if="showCustomAlert" class="fixed left-1/2 top-8 transform -translate-x-1/2 z-50">
         <div
@@ -60,7 +24,7 @@ async function fetchMovies(query = '') {
       </div>
     </transition>
     <div class="grid grid-cols-2 gap-4 p-4 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3">
-      <div v-for="movie in movies" :key="movie.id">
+      <div v-for="movie in results" :key="movie.id">
         <MovieCard :movie="movie"/>
       </div>
     </div>
