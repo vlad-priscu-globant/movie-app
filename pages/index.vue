e099702d517265f3c077a1f62ee0e5afeb8b8c8f
Dario Ispas <darioispas04@gmail.com>
2025-07-21 01:46:31 +0300

    ✨ feat: add skeleton loading for list and detail pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
+import { ref, watchEffect } from 'vue'
 import type { SearchResult, SearchResults } from "~/types/types";
+import SkeletonMovieCard from '@/components/SkeletonMovieCard.vue'
 
 const movies = ref<SearchResult[] | []>([])
 
-const {data, error} = await useFetch<SearchResults>('/api/movies')
+const {data, pending, error} = await useLazyFetch<SearchResults>('/api/movies')
 
 watchEffect(() => {
   if (data.value) {
@@ -14,9 +16,18 @@ watchEffect(() => {
 
 <template>
   <div class="grid grid-cols-2 gap-4 p-4 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3">
-    <template v-for="movie in movies" :key="movie.id">
-      <MovieCard :movie="movie"/>
-    </template>
+    <SkeletonMovieCard
+      v-if="pending && movies.length === 0"
+      v-for="n in 20"
+      :key="`skeleton-init-${n}`"
+    />
+
+    <MovieCard
+      v-else
+      v-for="movie in movies"
+      :key="movie.id"
+      :movie="movie"
+    />
   </div>
 </template>
 
