14af73f7d65e82f8c7a6e5cd37cf22039a06eecc
Bogdan Leica <bogdanleica77@gmail.com>
2025-07-25 15:25:43 +0300

    movie card design modification

--- components/MovieCard.vue
+++ components/MovieCard.vue
@@ -7,22 +7,23 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div  :key="props.movie.id"
-        class="rounded-lg overflow-hidden shadow hover:drop-shadow-[0_4px_12px_rgba(239,68,68,0.5)] transition hover:scale-110 duration-600">
+  <div :key="props.movie.id"
+    class="rounded-lg overflow-hidden shadow hover:drop-shadow-[0_4px_12px_rgba(0,0,0,1)] bottom-0 transition hover:scale-105 duration-600 hover:z-50 hover:-translate-y-8">
 
     <NuxtLink :to="`movie/${props.movie.id}`">
-      <div>
-        <img :alt="props.movie.title" :src="`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`"
-             class="w-full aspect-[2/3] object-cover" loading="lazy">
-        <div class="p-4">
-          <h2 class="text-lg font-semibold">{{ props.movie.title }}</h2>
-          <p class="text-sm text-grey-400">Release: {{ props.movie.release_date }}</p>
+      <div class="group relative ">
+        <div class="absolute inset-0 bg-gradient-to-t from-black/100 to-transparent opacity-60 group-hover:opacity-100 transition-opacity duration-500"></div>
+        <div>
+          <img :alt="props.movie.title" :src="`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`"
+            class="w-full aspect-[2/3] object-cover" loading="lazy">
+          <div class="p-4 absolute bottom-0 inset-x-0 text-center ">
+            <h2 class="text-xl font-semibold">{{ props.movie.title }}</h2>
+            <p class="text-sm text-grey-400">Release: {{ props.movie.release_date }}</p>
+          </div>
         </div>
       </div>
     </NuxtLink>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
